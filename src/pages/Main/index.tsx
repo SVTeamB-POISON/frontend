3c28bc1cf36db3736faa8b8d7f9f1b5ef2f5427f
sangminlee98 <tkdals0975@naver.com>
2023-01-06 21:09:34 +0900

    chore : 테스트 API 추가 및 proxy 코드 제거

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -4,15 +4,14 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 export default function Main() {
-  const { data: test } = useQuery<Test>([QueryKeys.TEST], () =>
-    restFetcher({ method: "GET", path: "test" }),
-  );
-  const { data: product } = useQuery(["PRODUCT"], () =>
+  const { data: product } = useQuery<Test[]>([QueryKeys.TEST], () =>
     restFetcher({ method: "GET", path: "/product" }),
   );
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <h1 className="text-3xl font-bold text-blue-600">{test}</h1>
+      <h1 className="text-3xl font-bold text-blue-600">
+        {JSON.stringify(product)}
+      </h1>
       <h1 className="text-3xl font-bold text-blue-600">
         {JSON.stringify(product)}
       </h1>
--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,6 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
-    proxy: {
-      "/api": "http://localhost:8001",
-    },
   },
   resolve: {
     alias: {
