e614ac3b49214f82427ae4c355c1154c6e883db3
robin <nowrobin3@gmail.com>
2023-01-10 14:48:18 +0900

    style : Logo title에 마진 추가

--- src/components/LogoTitle/index.tsx
+++ src/components/LogoTitle/index.tsx
@@ -7,7 +7,7 @@ export default function LogoTitle() {
       <div className={`flex flex-row ${styles.container}`}>
         <img className={`${styles.logo}`} src={logo} />
         <div
-          className={`flex flex-col justify-contents ${styles.textcontainer}`}
+          className={`flex flex-col justify-contents ${styles.textContainer}`}
         >
           <h1 className={`${styles.name}`}>Poison</h1>
           <p className={`${styles.subname}`}>독초 판별 사이트</p>
