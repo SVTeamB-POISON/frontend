6690840dad63f418f53794da36ff68d19277ba81
robin <nowrobin3@gmail.com>
2023-01-10 21:41:23 +0900

    refact : NavigationBar 태그 리팩토링 및 수정

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -1,23 +1,43 @@
 import styles from "./styles.module.scss";
 import logo from "@/assets/logo.svg";
 import search from "@/assets/search.svg";
+import {
+  ChangeEventHandler,
+  InputHTMLAttributes,
+  MouseEventHandler,
+  useState,
+} from "react";
 
 export default function NavigationBar() {
-  // function onClick(){
-  // }
+  const [flower, setFlower] = useState("");
 
+  function onClick(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    return console.log("clicked");
+  }
+
+  function onChange(event: React.FormEvent<HTMLInputElement>) {
+    setFlower(event.currentTarget.value);
+  }
   return (
     <>
-      <div className={`flex flex-col ${styles.navContainer}`}>
-        <div className={`flex flex-row ${styles.searchbar}`}>
-          <img className={`${styles.logoImg}`} src={logo}></img>
-          <h1 className={`${styles.logoName}`}>Poison</h1>
-          <div className={`flex flex-row bg-white ${styles.searchContainer}`}>
+      <div className={`flex flex-col ${styles.container}`}>
+        <div
+          className={`flex flex-row justify-between ${styles.searchSection}`}
+        >
+          <div className={`flex flex-row ${styles.title}`}>
+            <img className={`${styles.logoImg}`} src={logo}></img>
+            <h1 className={`${styles.logoName}`}>Poison</h1>
+          </div>
+          <div className={`flex flex-row  ${styles.searchContainer}`}>
             <input
-              className={`flex border-blue-600  ${styles.searchInput}`}
+              className={`flex ${styles.searchInput}`}
               placeholder="Type in the Flower Name"
+              onChange={onChange}
             />
-            <button className={`${styles.searchbtn}`}></button>
+            <button className={`${styles.searchbtn}`} onClick={onClick}>
+              <img src={search} />
+            </button>
           </div>
         </div>
         <div className={`bg-blue-600 ${styles.navline}`}></div>
