8ace381bebfe056277ba561addb500987d641227
robin <nowrobin3@gmail.com>
2023-01-14 05:16:09 +0900

    feat : rank modal 틀 구현

--- src/components/RankModal/index.tsx
+++ src/components/RankModal/index.tsx
@@ -1,47 +1,66 @@
 import styles from "./styles.module.scss";
 import image1 from "@/assets/image1.svg";
+import firstPlace from "@/assets/firstPlace.svg";
 export default function RankModal() {
   return (
-    <div className={styles.container}>
-      <div className={`flex flex-col ${styles.subContainer1}`}>
-        <div className={`flex flex-col${styles.first}`}>
-          <div></div>
+    <div className={`drop-shadow-2xl ${styles.container}`}>
+      <div className={`flex flex-row ${styles.subContainer1}`}>
+        <div className={`flex flex-col ${styles.third}`}>
+          <img className={`${styles.image}`} src={image1} />
+          <hr className={styles.line} />
+          <div className={`flex flex-row ${styles.labelContainer}`}>
+            <p>꽃이름</p>
+            <p>1200회</p>
+          </div>
         </div>
-        <div>2nd,3rd</div>
+        <div className={`flex flex-col ${styles.first}`}>
+          <img src={firstPlace} className={styles.firstCrown} />
+          <img className={`${styles.image}`} src={image1} />
+          <hr className={styles.line} />
+          <div className={`flex flex-row ${styles.labelContainer}`}>
+            <p>꽃이름</p>
+            <p>1200회</p>
+          </div>
+        </div>
+        <div className={`flex flex-col ${styles.second}`}>
+          <img className={`${styles.image}`} src={image1} />
+          <hr className={styles.line} />
+          <div className={`flex flex-row ${styles.labelContainer}`}>
+            <p>꽃이름</p>
+            <p>1200회</p>
+          </div>
+        </div>
       </div>
       <div className={styles.subContainer2}>
-        <ul>
-          <div className={`flex flex-col ${styles.listRank}`}>
-            <div className={`flex flex-row ${styles.item}`}>
-              <a className={`flex flex-row ${styles.anchor}`} href="/Detail">
-                <img className={`${styles.image}`} src={image1} />
-                <p>순위</p>
-                <p>이름</p>
-                <p>2000회</p>
-              </a>
-            </div>
+        <div className={`flex flex-col ${styles.listRank}`}>
+          <div className={`flex flex-col ${styles.item}`}>
+            <a className={`flex flex-row ${styles.anchor}`} href="/Detail">
+              <img className={`${styles.image}`} src={image1} />
+              <p>순위</p>
+              <p>이름</p>
+              <p>2000회</p>
+            </a>
+            <hr className={styles.line2} />
           </div>
-          <div className={`flex flex-col ${styles.listRank}`}>
-            <div className={`flex flex-row ${styles.item}`}>
-              <a className={`flex flex-row ${styles.anchor}`} href="/Detail">
-                <img className={`${styles.image}`} src={image1} />
-                <p>순위</p>
-                <p>이름</p>
-                <p>2000회</p>
-              </a>
-            </div>
+          <div className={`flex flex-col ${styles.item}`}>
+            <a className={`flex flex-row ${styles.anchor}`} href="/Detail">
+              <img className={`${styles.image}`} src={image1} />
+              <p>순위</p>
+              <p>이름</p>
+              <p>2000회</p>
+            </a>
+            <hr className={styles.line2} />
           </div>
-          <div className={`flex flex-col ${styles.listRank}`}>
-            <div className={`flex flex-row ${styles.item}`}>
-              <a className={`flex flex-row ${styles.anchor}`} href="/Detail">
-                <img className={`${styles.image}`} src={image1} />
-                <p>순위</p>
-                <p>이름</p>
-                <p>2000회</p>
-              </a>
-            </div>
+          <div className={`flex flex-col ${styles.item}`}>
+            <a className={`flex flex-row ${styles.anchor}`} href="/Detail">
+              <img className={`${styles.image}`} src={image1} />
+              <p>순위</p>
+              <p>이름</p>
+              <p>2000회</p>
+            </a>
+            <hr className={styles.line2} />
           </div>
-        </ul>
+        </div>
       </div>
     </div>
   );
