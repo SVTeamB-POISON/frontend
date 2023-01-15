13a716824adf9c8d8581ed10771d5dff043b0fab
AlgeMoya <kjhobd2@naver.com>
2023-01-16 01:56:37 +0900

    feat: 결과페이지 데이터 상세보기 모달창 구현

--- src/components/DetailModal/index.tsx
+++ src/components/DetailModal/index.tsx
@@ -1,45 +1,61 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import icon_x from "@/assets/icon_x.png";
-import sampleImg from "@/assets/image1.svg";
 import { DetailData } from "@/types/detail";
-import { QueryKeys, restFetcher } from "@/queryClient";
-import { useQuery } from "@tanstack/react-query";
 
 type DetailModalProps = {
+  close: () => void;
+  children: React.ReactNode;
   detail: DetailData;
 };
 
-export default function DetailModal({ detail }: DetailModalProps) {
-  return (
-    <div className={styles.container} style={{ backgroundColor: "#373261" }}>
-      <div className={styles.summary}>
-        <img
-          className={styles.imageSection}
-          src={detail.s3_url}
-          alt="flowerimage"
-        />
+function DetailModal(props: DetailModalProps) {
+  // const [animate, setAnimate] = useState(false);
+  // const [localVisible, setLocalVisible] = useState(visible);
 
-        <div className={styles.simpleDesc}>
-          <div className={styles.dataHeader}>
-            <h1 className={styles.dataName}>{detail.name}</h1>
-            <h1 className={styles.dataToxic}>
-              독성 여부: {detail.poison ? "독초" : "없음"}
-            </h1>
-            <div className={styles.dataDesc}>
-              {detail.poison && <p>중독 시 증상: {detail.symptom}</p>}
-              <p>학명: {detail.scientific_name}</p>
-              <p>꽃말: {detail.flower_language}</p>
+  // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
+  const { close, detail } = props;
+
+  return (
+    // 모달이 열릴 때 openModal 클래스가 생성된다.
+    <div
+      className={styles.modal}
+      style={{ backgroundColor: detail.poison ? "#373261" : "#6C7D6C" }}
+    >
+      <section>
+        <div className={styles.summary}>
+          <img
+            className={styles.imageSection}
+            src={detail.s3_url}
+            alt="flowerimage"
+          />
+          <div className={styles.simpleDesc}>
+            <div className={styles.dataHeader}>
+              <h1 className={styles.dataName}>{detail.name}</h1>
+              <h1 className={styles.dataToxic}>
+                독성 여부: {detail.poison ? "독초" : "없음"}
+              </h1>
+              <div className={styles.dataDesc}>
+                {detail.poison && <p>중독 시 증상: {detail.symptom}</p>}
+                <p>학명: {detail.scientific_name}</p>
+                <p>꽃말: {detail.flower_language}</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className={styles.closeButton}>
-        <img src={icon_x} alt="closebutton" />
-      </div>
-      <div className={styles.detailDesc}>
-        <p>{detail.detail}</p>
-      </div>
+        <div
+          className={styles.closeButton}
+          onClick={close}
+          style={{ backgroundColor: detail.poison ? "#f99ee6" : "#8DC36C" }}
+        >
+          <img src={icon_x} alt="closebutton" />
+        </div>
+        <div className={styles.detailDesc}>
+          <p>{detail.detail}</p>
+        </div>
+      </section>
     </div>
   );
 }
+
+export default DetailModal;
--- src/pages/Result/index.tsx
+++ src/pages/Result/index.tsx
@@ -1,13 +1,60 @@
+import { useEffect, useState } from "react";
 import NavigationBar from "@/components/NavigationBar";
 import ResultCard from "@/components/ResultCard";
+import DetailModal from "@/components/DetailModal";
 import { ResultData } from "@/types/result";
+import { DetailData } from "@/types/detail";
 import { motion } from "framer-motion";
 import { useLocation } from "react-router-dom";
 import styles from "./styles.module.scss";
+import { QueryKeys, restFetcher } from "@/queryClient";
+import { useQueries, useQuery } from "@tanstack/react-query";
+import Loading from "@/components/Loading";
+
+type RouterState = {
+  data: ResultData[];
+};
 
 export default function ResultPage() {
   const location = useLocation();
-  const data = (location.state as RouterState).data;
+  // ResultCard에 onClick 함수랑 setState 함수 props로 전달해주기
+  const resultdata = (location.state as RouterState).data; // 결과창 3개 들어가는거
+  const details = useQueries({
+    queries: resultdata.map((data) => {
+      return {
+        queryKey: [data.name],
+        queryFn: () =>
+          restFetcher({
+            method: "GET",
+            path: "/flowers/details",
+            params: { name: data.name },
+          }),
+      };
+    }),
+  });
+
+  // useState를 사용하여 open 상태를 변경한다. (open일 때 true로 만들어 열리는 방식)
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalData, setModalData] = useState<DetailData>();
+
+  const onFilterDetail = (name: string) => {
+    const filteredData = details.filter((res) => res.data.name === name);
+    return filteredData[0].data;
+  };
+
+  // onClick 함수로 openModal 선언
+  const openModal = (name: string) => {
+    setModalOpen(true);
+    document.body.style.overflow = "hidden";
+    const data = onFilterDetail(name);
+    setModalData(data);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+    document.body.style.overflow = "unset";
+  };
+
   return (
     <div className={styles.container}>
       <NavigationBar />
@@ -25,11 +72,21 @@ export default function ResultPage() {
           <p>독초일 수 있어요!</p>
         </div>
         <ul className={styles.resultContainer}>
-          {data?.map((result, idx) => (
-            <ResultCard key={idx} result={result} />
+          {resultdata?.map((result, idx) => (
+            <ResultCard
+              key={idx}
+              result={result}
+              onClick={() => openModal(result.name)}
+            />
           ))}
         </ul>
       </motion.div>
+
+      {modalOpen && (
+        <DetailModal close={closeModal} detail={modalData!}>
+          children
+        </DetailModal>
+      )}
     </div>
   );
 }
