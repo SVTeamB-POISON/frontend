8dc29a871b9c9203329e6ffd9ae4b7e588c0568d
sangminlee98 <tkdals0975@naver.com>
2023-01-18 23:27:58 +0900

    fix : 깃허브액션 테스트

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -185,7 +185,7 @@ function Content({
         </div>
         {isFileUploaded ? (
           <button className={styles.uploadbtn} onClick={handleUpload}>
-            업로두
+            업로드
           </button>
         ) : (
           <p>파일 선택 또는 드래그</p>
